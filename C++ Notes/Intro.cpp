f59da3f711d4632f7c550deb507400289266a4b3
Stellin <stellinjohn2003@gmail.com>
2026-06-19 17:34:02 +0400

    Add introductory C++ examples for console output

---
+++ C++ Notes/Intro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!";
+    
+    int num = 15;
+    cout << "The value of num is: " << num << endl;
+    
+    return 0;
+}
+
+
+int main(){
+
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
---
+++ C++ Notes/L02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// This is to test printing of a number to the console.
+int main(){
+
+    cout << 3;
+    cout << 3 + 3;
+    cout << 2 * 5;
+    return 0;
+}
+
+//  Both \n and endl are used to break lines. However, \n is most used.
+//  \t	Creates a horizontal tab
+//  \\	Inserts a backslash character (\)
+//  \"	Inserts a double quote character
